a9f43682dbf432fe23db9f307cd355cfd38816cb
NlightN22 <oncharterliz@gmail.com>
2024-10-23 02:07:15 +0700

    test

--- AD/CheckUserLogon.ps1
+++ AD/CheckUserLogon.ps1
@@ -2,15 +2,14 @@ param (
     [int]$days
 )
 
-$DateThreshold = (Get-Date).Adddays(-$days)
+# Get the current date
+$currentDate = Get-Date
 
-$users = Get-ADUser -Filter * -Property LastLogonDate
-
-foreach ($user in $users) {
-    if (-not $user.LastLogonDate -or ($user.LastLogonDate -lt $DateThreshold)) {
-        [PSCustomObject]@{
-            UserName       = $user.SamAccountName
-            LastLogonDate  = if ($user.LastLogonDate) { $user.LastLogonDate } else { "Never" }
-        }
-    }
+# Find all users in the domain
+$users = Get-ADUser -Filter * -Property LastLogonTimestamp | Where-Object {
+    # Users who never logged on (LastLogonTimestamp is null) or who haven't logged on in the last 'days' days
+    ($_).LastLogonTimestamp -eq $null -or ($currentDate - [DateTime]::FromFileTime($_.LastLogonTimestamp)).Days -gt $days
 }
+
+# Output the list of inactive users
+$users | Select-Object Name, @{Name="LastLogonDate";Expression={[DateTime]::FromFileTime($_.LastLogonTimestamp)}} | Format-Table -AutoSize
